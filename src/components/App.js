f74c8c2c6f21d894a8e738a6a562e2059ff6c924
gourav <gouravthakur525@gmail.com>
2021-05-16 19:23:44 +0530

    new code

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+const name=[{id:"relativeListItem1",k:"gourav"},
+            {id:"relativeListItem2",k:"vinay"}];
+  const listitem=name.map((names)=>
+  <li key={names.id}>{names.k}</li>
+  );
         return(
             <div id="main">
                {/* Do not remove the main div */}
+             <ol key={name.id}>{listitem}</ol>
+             <ol key={"relativeList"}>{listitem}</ol>
             </div>
         )
     }
